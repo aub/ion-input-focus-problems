254cb87b9ffeef48158acf777562525cc4319933
Aubrey Holland <aubreyholland@gmail.com>
2020-02-26 15:15:49 -0500

    initial commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ExploreContainer.css';
 
+import { IonButton, IonInput } from '@ionic/react';
+
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
@@ -8,6 +10,8 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
     <div className="container">
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonInput autofocus placeholder="placeholdin" />
+      <IonButton>PRESS ME</IonButton>
     </div>
   );
 };
